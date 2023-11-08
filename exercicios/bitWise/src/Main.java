3fd550d54a51ea18e36597dc605ff6978c9e0206
Valmir Gama <111182775+ValmirSGama@users.noreply.github.com>
2023-11-08 20:14:20 -0300

    Add File via upload

---
+++ exercicios/bitWise/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // Declarando duas variáveis inteiras n1 e n2 e atribuindo valores a elas
+        int n1 = 89;
+        int n2 = 60;
+        
+        // Imprimindo o resultado da operação bitwise "AND" entre n1 e n2
+        System.out.println(n1 & n2);
+        
+        // Imprimindo o resultado da operação bitwise "OR" entre n1 e n2
+        System.out.println(n1 | n2);
+        
+        // Imprimindo o resultado da operação bitwise "XOR" (OU exclusivo) entre n1 e n2
+        System.out.println(n1 ^ n2);
+    }
+
+}
